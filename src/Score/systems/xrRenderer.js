bd88a4300cc1889630eb7eee04a982fe84352928
nari <nari@itu.dk>
2024-11-27 08:09:44 +0100

    chnaged reference space type to "unbounded"

--- src/Score/systems/xrRenderer.js
+++ src/Score/systems/xrRenderer.js
@@ -5,13 +5,13 @@ import { ARButton } from "three/examples/jsm/webxr/ARButton.js";
 function createXRRenderer() {
   const renderer = new WebGLRenderer({ antialias: true, alpha: true }); // TODO: use this for AR
   renderer.xr.enabled = true; // TODO: Enable WebXR
-  renderer.xr.setReferenceSpaceType("viewer"); // TODO: use this for AR
+  renderer.xr.setReferenceSpaceType("unbounded"); // TODO: use this for AR
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
 
   document.body.appendChild(
     ARButton.createButton(renderer, {
-      requiredFeatures: ["anchors", "local-floor"],
+      requiredFeatures: ["anchors"],
       optionalFeatures: ["light-estimation", "depth-sensing", "bounded-floor"],
       depthSensing: {
         usagePreference: ["gpu-optimized"],
